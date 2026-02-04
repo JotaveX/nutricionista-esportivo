3abaa44e2f65e3a8f943c1b0ee3ad0f647f27c0a
JoaoVictorNobuntu <joaovictor@nobuntu.com.br>
2026-02-04 00:40:28 -0300

    Fix: test db connection

--- backend/src/database.ts
+++ backend/src/database.ts
@@ -1,6 +1,8 @@
 import { Pool } from 'pg';
 import dotenv from "dotenv";
 
+dotenv.config()
+console.log(process.env.DATABASE_URL)
 // Conexão com Postgres
 const pool = new Pool({
     connectionString: process.env.DATABASE_URL,
